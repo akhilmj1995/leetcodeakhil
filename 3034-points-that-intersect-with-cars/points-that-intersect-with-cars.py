022c5b96cb153e5f5917dbff9d93724732e1fa74
AKHIL MOHAN <150775336+akhilmj1995@users.noreply.github.com>
2024-01-31 10:30:27 +0530

    Time: 71 ms (86.33%) | Memory: 16.6 MB (53.33%) - LeetSync

--- 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.py
+++ 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.py
@@ -2,11 +2,8 @@ class Solution:
     def numberOfPoints(self, nums: List[List[int]]) -> int:
            cord=[]
            for i in range(len(nums)):
-               k=nums[i][0]-nums[i][1]
-               a=nums[i][0]
-               while a<=nums[i][1]:
-                   cord.append(a)
-                   a+=1
+               for j in range(nums[i][0],nums[i][1]+1):
+                   cord.append(j)    
            return len(set(cord))
            
         
