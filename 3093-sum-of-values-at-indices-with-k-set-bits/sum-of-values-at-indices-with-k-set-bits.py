e47ba589e79b3d5d9f4e26f73643bcbf98d9f1e6
AKHIL MOHAN <150775336+akhilmj1995@users.noreply.github.com>
2024-02-01 10:30:24 +0530

    Time: 76 ms (57.02%) | Memory: 16.8 MB (57.02%) - LeetSync

--- 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.py
+++ 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.py
@@ -2,7 +2,7 @@ class Solution:
     def sumIndicesWithKSetBits(self, nums: List[int], k: int) -> int:
         a=[]
         for i in range(len(nums)):
-            ans=bin(i).replace('0b','00')
+            ans=bin(i).replace('0b','')
             if ans.count('1')==k:
                 a.append(i)
         sum=0
