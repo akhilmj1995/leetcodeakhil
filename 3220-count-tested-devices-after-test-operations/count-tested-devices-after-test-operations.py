e3070bca2ea15a90b11288355601374e5ad778bc
AKHIL MOHAN <150775336+akhilmj1995@users.noreply.github.com>
2024-02-21 09:44:21 +0530

    Time: 68 ms (45.44%) | Memory: 16.5 MB (96.28%) - LeetSync

---
+++ 3220-count-tested-devices-after-test-operations/count-tested-devices-after-test-operations.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countTestedDevices(self, batteryPercentages: List[int]) -> int:
+        count=0
+        for i in range(len(batteryPercentages)):
+            if batteryPercentages[i]>0:
+                count+=1
+                for j in range(i+1,len(batteryPercentages)):
+                    batteryPercentages[j]-=1
+                continue
+            else:
+                continue
+        return count
