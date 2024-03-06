9585d7cff8824de1a059d6f62fe6124cc19b0c76
AKHIL MOHAN <150775336+akhilmj1995@users.noreply.github.com>
2024-03-06 15:11:03 +0530

    Time: 46 ms (61.66%) | Memory: 16.4 MB (96.04%) - LeetSync

--- 3324-split-the-array/split-the-array.py
+++ 3324-split-the-array/split-the-array.py
@@ -1,7 +1,7 @@
 class Solution:
     def isPossibleToSplit(self, nums: List[int]) -> bool:
-        for i in range(len(nums)):
-            if nums.count(nums[i])>2:
+        for i in nums:
+            if nums.count(i)>2:
                 return False
         return True
 
