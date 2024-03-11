93205de693d3b9882cdbd7d96ed39e08a19143b7
AKHIL MOHAN <150775336+akhilmj1995@users.noreply.github.com>
2024-03-11 20:30:37 +0530

    Time: 145 ms (93.09%) | Memory: 18.6 MB (14.73%) - LeetSync

--- 645-set-mismatch/set-mismatch.py
+++ 645-set-mismatch/set-mismatch.py
@@ -1,17 +1,7 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        a=[]
-        for i in range(len(nums)):
-            if nums.count(nums[i])==2:
-                if nums[i] not in a:
-                     a.append(nums[i])
-                     break
-        for i in range(len(nums)):
-            if i+1 not in nums:
-                a.append(i+1)
-                break
-        return a
-            
-                
+        b=sum(nums)-sum(set(nums))
+        c=sum(range(1,len(nums)+1))-sum(set(nums))
+        return [b,c]       
         
                 
