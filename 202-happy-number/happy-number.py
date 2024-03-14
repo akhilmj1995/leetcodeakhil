937626db050cb98f8b2076b8e306608b390ca50a
AKHIL MOHAN <150775336+akhilmj1995@users.noreply.github.com>
2024-03-14 14:30:59 +0530

    Time: 31 ms (89.70%) | Memory: 16.6 MB (45.39%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -5,7 +5,6 @@ class Solution:
            k=0
            for i in str(n):
              k+=int(i)**2
-             print(k)
            if k==1:
               return True
            if k in array:
