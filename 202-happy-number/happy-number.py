43ce4811a2dd87f8b20492ec33e2df4780467ca6
AKHIL MOHAN <150775336+akhilmj1995@users.noreply.github.com>
2024-03-14 14:30:30 +0530

    Time: 39 ms (45.00%) | Memory: 16.7 MB (19.52%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,14 +1,16 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        a=0
-        while a<10:
+        array=[]
+        while True:
            k=0
            for i in str(n):
              k+=int(i)**2
              print(k)
            if k==1:
               return True
+           if k in array:
+             return False
+           array.append(k)
            n=k
-           a+=1  
-        return False
+
 
