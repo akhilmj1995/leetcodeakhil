40413a08d59b885eed8574392a51a3a84e6c59b2
AKHIL MOHAN <150775336+akhilmj1995@users.noreply.github.com>
2024-03-30 15:20:03 +0530

    Time: 53 ms (38.05%) | Memory: 17.6 MB (17.69%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -4,7 +4,6 @@ class Solution:
         n=list(t)
         m.sort()
         n.sort()
-        if set(list(s))==set(list(t)) and len(s)==len(t):
-            if m==n:     
-                return True
+        if m==n:     
+            return True
         return False
