551ebd7673f41b7d830bd5945975f58010225722
AKHIL MOHAN <150775336+akhilmj1995@users.noreply.github.com>
2024-05-18 12:52:46 +0530

    Time: 33 ms (88.09%) | Memory: 16.6 MB (40.16%) - LeetSync

---
+++ 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.py
@@ -0,0 +1,9 @@
+class Solution:
+    def dominantIndex(self, nums: List[int]) -> int:
+        new_array=nums.copy()
+        new_array.sort()
+        print(nums)
+        print(new_array)
+        if new_array[-1]>=2*new_array[-2]:
+            return nums.index(new_array[len(new_array)-1])
+        return -1
